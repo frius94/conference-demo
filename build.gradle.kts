6feaa87f5b6edfe76481cc0009d6831594eecb42
Umut Savas <daniel230994@gmail.com>
2022-07-01 00:09:08 +0200

    added openapi dependency, added openApiGenerate task settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.openapi.generator") version "6.0.0"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
@@ -26,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.security:spring-security-test")
 	implementation("com.ibm.mq:mq-jms-spring-boot-starter:2.6.7")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -41,6 +43,15 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+openApiGenerate {
+	generatorName.set("kotlin")
+	inputSpec.set("$rootDir/src/main/resources/api-docs.yaml")
+	outputDir.set("$buildDir/generated")
+	apiPackage.set("com.pluralsight.conferencedemo.api")
+	invokerPackage.set("com.pluralsight.conferencedemo.invoker")
+	modelPackage.set("com.pluralsight.conferencedemo.model")
+}
+
 val jar by tasks.getting(Jar::class) {
 	manifest {
 		attributes["Main-Class"] = "com.pluralsight.conferencedemo.ConferenceDemoApplication"
@@ -50,3 +61,8 @@ val jar by tasks.getting(Jar::class) {
 springBoot {
 	mainClass.set("com.pluralsight.conferencedemo.ConferenceDemoApplicationKt")
 }
+
+// to get api documentation open this: http://localhost:8080/swagger-ui/index.html
+// to get json open this: http://localhost:8080/v3/api-docs
+// to get yaml open this: http://localhost:8080/v3/api-docs.yaml
+// to generate classes from yaml file add openApiGenerate in build.gradle and run gradle openApiGenerate
